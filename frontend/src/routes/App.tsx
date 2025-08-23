0d04b6cb76abeaefc5f0978bcf00cfe2501949bc
Kritiraj <kritiraj.tech@gmail.com>
2025-08-23 12:46:09 +0530

    auth setup

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -1,15 +1,23 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
-import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "@/pages/NotFoundPage";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
+
 function App() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div className="flex h-screen items-center justify-center">Loading...</div>;
+  }
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="theme">
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </ThemeProvider>
   );
